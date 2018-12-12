d996d5d794a51e6e4323dc475d45aa401445fa7d
Tyson Frederick <tkinetik1@gmail.com>
2018-12-11 19:34:34 -0800

    Update app level gradle file, sync completed, changed/updated sdk build tools

--- app/src/main/java/com/tysonfrederick/storyteller/MainActivity.java
+++ app/src/main/java/com/tysonfrederick/storyteller/MainActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- package com.google.firebase.example.fireeats;
+ package com.tysonfrederick.storyteller;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
